0e314046a54f1ba7fea927e06f037f8ff839984d
fabross <fabricio.rosales.12@sansano.usm.cl>
2023-06-05 23:28:19 -0400

    ordenamiento final para entrega de benchmarking

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Implementation of insulator classification, for Computer Vision project.
 * Platform: Python (OpenCV)
 * Dataset: https://github.com/SFStefenon/InsulatorsDataSet
 * Main reference: https://ietresearch.onlinelibrary.wiley.com/doi/10.1049/gtd2.12353
+
+## Notebooks usage
+Requirements to use the notebooks:
+1. Clone this repository
+2. Clone Dataset repository at the same level of this project
+3. Create a virtual env (optional) and run `pip install requirements.txt`
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,26 @@
 import pandas as pd
 from .pre_processing import Pre_processing
 import os
+import numpy as np
 
 def load_data(data_dir = '../InsulatorsDataSet/'):
+    """Return dataset preprocessed
+
+    Parameters
+    ----------
+    data_dir : str, optional
+        data set directory, by default '../InsulatorsDataSet/'
+
+    Returns
+    -------
+    array
+        x_train, x_test, y_train, y_test
+    """
     img_path = []
     img_data = []
     labels = []
     # extraction_features in format list( (extraction, kaolin(g/l)) )
-    extraction_features = [('03','16'),('04','8'),('05','20'),('06','10'),('07', '6'),('08','25')]
+    extraction_features = [('03',3),('04',1),('05',4),('06',2),('07', 0),('08',5)]
     for i in range(len(extraction_features)):
         idx = extraction_features[i][0]
         kaolin = extraction_features[i][1]
@@ -16,7 +29,9 @@ def load_data(data_dir = '../InsulatorsDataSet/'):
             for img in os.listdir(data_dir+idx+' Extraction/'+insulators_dir):
                 if img.split("_")[1] == "35.jpg":
                     pre_processing = Pre_processing(data_dir+idx+' Extraction/'+insulators_dir+"/"+img)
-                    img_data += pre_processing.operate()
+                    pre_processing.operate()
+                    for result in pre_processing.results:
+                        img_data.append(result.reshape(-1))
                     img_path += [data_dir+idx+' Extraction/'+insulators_dir+"/"+img]*5
                     labels += [kaolin]*5
-    return pd.DataFrame({'img': img_path, 'preprocessed': img_data , 'label':labels})
+    return img_data, labels
